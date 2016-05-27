a01b005b852e3b2e4e565a81385538f965a0836f
Michael Sitter <michael.sitter@gonitro.com>
2016-05-27 11:02:37 -0700

    0.1.1

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _customMatchers = require('./customMatchers.js');
+
+var _customMatchers2 = _interopRequireDefault(_customMatchers);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+	customMatchers: _customMatchers2.default
+};
